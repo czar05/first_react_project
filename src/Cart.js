2524047aadb5dc5cf8fab06531ec9369e8daa30f
deepak <patel.deepak750@gmail.com>
2021-09-27 13:19:16 +0530

    added props

--- src/Cart.js
+++ src/Cart.js
@@ -3,12 +3,50 @@ import CartItem from "./CartItem";
 
 
 class Cart extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            products : [
+                {
+                    price: 99,
+                    title: 'Watch',
+                    qty: 1,
+                    img: '',
+                    id : 1
+                },
+                {
+                    price: 999,
+                    title: 'Phone',
+                    qty: 5,
+                    img: '',
+                    id : 2
+                },
+                {
+                    price: 999,
+                    title: 'Laptop',
+                    qty: 3,
+                    img: '',
+                    id : 3
+                }
+            ]
+        }
+       
+    }
    render() {
+       const {products} = this.state;
+      
        return (
            <div className="cart">
-               <CartItem />
-               <CartItem />
-               <CartItem />
+              
+              {products.map((product) => {
+               return (
+               <CartItem
+               product={product} 
+               key={product.id}
+                />
+               )
+              })}
+              
            </div>
        );
    }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,7 @@
 import React from "react";
 
 class CartItem extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-       
-    }
+   
 
     increaseQuantity = ()=> {
         
@@ -39,7 +30,8 @@ class CartItem extends React.Component {
         });
     }
     render () {
-        const{price, title, qty} = this.state;
+        console.log('this.props', this.props)
+        const{price, title, qty} = this.props.product;
         return (
             <div className="cart-item">
                <div className="left-block">
